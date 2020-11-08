0e0cfe6a72aedd05ddab6d9cf8faf7d4ee20bcfd
Denis Valeev <valeevden@gmail.com>
2020-11-08 23:34:36 +0300

    add 7 new task

--- ConsoleForTest/Program.cs
+++ ConsoleForTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SelfMadeList;
 
 namespace ConsoleForTest
 {
@@ -6,7 +7,40 @@ namespace ConsoleForTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array;
+            array = new int[6];
+
+            Random r = new Random();
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(-30, 30); // Наполняем массив рандомными значениями
+                Console.Write(array[i] + " "); // выводим для наглядности
+            }
+
+            int[] array2 = SelfMadeList.ArrayList.DecreaseByIndex(array, 2);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+
+                Console.Write(array2[i] + " "); // выводим для наглядности
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(array.Length);
+            Console.WriteLine(array2.Length);
+
+            //ArrayList artest = new ArrayList();
+            //artest.Add(3);
+            //artest.AddToStart(5);
+            //int f = artest.DlinaSpiska;
+            //Console.WriteLine(f);
+
+
         }
     }
 }
--- SelfMadeList/ArrayList.cs
+++ SelfMadeList/ArrayList.cs
@@ -23,18 +23,44 @@ namespace SelfMadeList
             _array = new int[9];
             ListLength = 0;
         }
+        // Метод возвращает длинну Списка
+        public int GetLength() 
+        {
+          return ListLength;
+        }
+
+        // Метод возвращает значение по Индексу
+        public int GetIndexValue(int index)
+        {
+            int value = _array[index];
+            return value;
+        }
+
+        // Метод. Возвращает индекс по Значению
+        public int GetValueIndex(int value)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] == value)
+                {
+                    return i;
+                }
+            }
+            throw new Exception("No match Found");
+        }
+
 
         // Метод. Добавляет 1 значение в конец массива. При необходимости увеличивает размер массива вложенным методом. Изменяет длинну Листа на 1 элемент
         public void Add(int value)
         {
-            if (_arrayLength >= ListLength)
+            if (_arrayLength <= ListLength)
             {
                 IncreaseListLength();
             }
             _array[ListLength] = value;
             ListLength++;
         }
-        // Метод. Добавляет 1 значение в начало массива. При необходимости увеличивает размер массива вложенным методом. Изменяет длинну Листа на 1 элемент
+        // Метод. Добавляет 1 значение в начало массива вложенным методом. При необходимости увеличивает размер массива вложенным методом. Изменяет длинну Листа на 1 элемент
         public void AddToStart(int value)
         {
             if (_arrayLength <= ListLength)
@@ -44,7 +70,55 @@ namespace SelfMadeList
             _array = MoveArrayForwardFrom0toValue(_array, value);
             ListLength++;
         }
-        // Метод. Увеличивает размер массива на number элементов
+
+        // Метод. Удаляет с конца один элемент. При необходимости сокращает размер массива вложенным методом. Изменяет длинну Листа на -1 элемент
+        public void Del()
+        {
+            _array = ShrinkLastElement(_array);
+            ListLength--;
+            if (_arrayLength >= ListLength)
+            {
+                DecreaseListLength();
+            }
+        }
+
+        // Метод. Удаляет с начала один элемент. При необходимости сокращает размер массива вложенным методом. Изменяет длинну Листа на -1 элемент
+        public void DelFirstElement()
+        {
+            _array = ShrinkFistElement(_array);
+            ListLength--;
+
+            if (_arrayLength >= ListLength)
+            {
+                DecreaseListLength();
+            }
+        }
+
+        // Метод. Удаляет элемент по индексу. При необходимости сокращает размер массива вложенным методом. Изменяет длинну Листа на -1 элемент
+        public void DelIndexElement(int index)
+        {
+            _array = DecreaseByIndex(_array, index);
+            ListLength--;
+
+            if (_arrayLength >= ListLength)
+            {
+                DecreaseListLength();
+            }
+        }
+
+
+        // Метод. Добавляет 1 значение в список по индексу вложенным методом. При необходимости увеличивает размер массива вложенным методом. Изменяет длинну Листа на 1 элемент
+        public void AddToIndex(int index, int value)
+        {
+            if (_arrayLength <= ListLength)
+            {
+                IncreaseListLength();
+            }
+            _array = MoveArrayForwardFromIndex(_array, index);
+            _array[index] = value; ;
+            ListLength++;
+        }
+        // Метод. Увеличивает размер массива и листа на number элементов
         private void IncreaseListLength(int number = 1)
         {
             int newListLength = _arrayLength;
@@ -59,6 +133,22 @@ namespace SelfMadeList
             _array = newArray;
         }
 
+        // Метод. Уменьшает размер Листа и массива
+        private void DecreaseListLength()
+        {
+            int newListLength = _arrayLength;
+            while (newListLength *2 > ListLength )
+            {
+                newListLength = (int)(newListLength * 0.67 );
+            }
+
+            int[] newArray = new int[newListLength];
+            Array.Copy(_array, newArray, _arrayLength);
+
+            _array = newArray;
+        }
+
+        //Пересоздание метода Assert.Equals для тестов
         //public override bool Equals(object obj)
         //{
         //    ArrayList arrayList = (ArrayList)obj;
@@ -80,6 +170,7 @@ namespace SelfMadeList
         //    return true;
         //}
 
+        // Метод. Сдвигает массив вперед на value элементов
         private int[] MoveArrayForwardFrom0toValue(int[] _array, int value)
         {
             int[] newArray = new int[_array.Length + value];
@@ -92,5 +183,56 @@ namespace SelfMadeList
             return _array;
         }
 
+        // Метод. Сдвигает массив вперед на 1 элемент начиная с определенного индекса
+        public static  int[] MoveArrayForwardFromIndex(int[] _array, int index)
+        {
+            int[] newArray = new int[_array.Length + 1];
+            Array.Copy(_array, newArray, _array.Length);
+                    
+            for (int i = index; i < _array.Length; i++)
+            {
+                newArray[i + 1] = _array[i];
+            }
+            _array = newArray;
+            return _array;
+        }
+
+        // Метод. Удаляет из массива последний элемент
+        public static int[] ShrinkLastElement(int[] _array)
+        {
+            int[] newArray = new int[_array.Length - 1];
+            Array.Copy(_array, newArray, _array.Length-1);
+            _array = newArray;
+            return _array;
+        }
+
+        // Метод. Удаляет из массива первый элемент
+        public static int[] ShrinkFistElement(int[] _array)
+        {
+            int[] newArray = new int[_array.Length - 1];
+            for (int i = 1; i < _array.Length; i++)
+            {
+                newArray[i-1] = _array[i];
+            }
+
+            _array = newArray;
+            return _array;
+        }
+
+        // Метод. Удаляет из массива элемент по индексу
+        public static int[] DecreaseByIndex(int[] _array, int index)
+        {
+            int[] newArray = new int[_array.Length - 1];
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = _array[i];
+            }
+            for (int i = _array.Length-1; i > index; i--)
+            {
+                 newArray[i-1] = _array[i];
+            }
+            _array = newArray;
+            return _array;
+        }
     }
 }
